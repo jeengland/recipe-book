57f450610290993f8484c5659316617e3b68556c
Jacob Cavazos-England <jacob.cavazos-england@wunderkind.co>
2022-06-23 17:20:01 -0400

    Added recipe C/U/D functionality

--- backend/database/routers/recipeRouter.js
+++ backend/database/routers/recipeRouter.js
@@ -15,4 +15,85 @@ router.get('/', (req, res) => {
 		});
 });
 
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+
+	db.getRecipeById(id)
+		.then(recipe => {
+			if (recipe[0]) {
+				res.status(200).json({
+					data: recipe[0]
+				});
+			} else {
+				res.status(404).json({
+					error: `Could not find recipe by id "${id}"`
+				});
+			}
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			});
+		});
+});
+
+router.post('/', (req, res) => {
+	const recipe = req.body;
+
+	db.addRecipe(recipe)
+		.then(recipe => {
+			res.status(200).json({
+				data: recipe
+			});
+		})
+		.catch(err => {
+			res.status(400).json({
+				error: err
+			});
+		});
+});
+
+router.put('/:id', (req, res) => {
+	const id = req.params,
+		changes = req.body;
+
+	db.getRecipeById(id)
+		.then(recipe => {
+			if (recipe) {
+				db.updateRecipe(id, changes)
+					.then(updatedRecipe => {
+						res.status(200).json({
+							data: updatedRecipe
+						});
+					})
+					.catch(err => {
+						res.status(400).json({
+							error: err
+						});
+					});
+			} else {
+				res.status(404).json({ error: `Could not find recipe by id "${id}"` });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ error: err });
+		});
+});
+
+router.delete('/:id', (req, res) => {
+	const { id } = req.params;
+
+	db.removeRecipe(id)
+		.then(deleted => {
+			if (deleted) {
+				res.json({ removed: deleted });
+			} else {
+				res.status(404).json({ error: `Could not find recipe by id "${id}"` });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ error: err });
+		});
+});
+
 module.exports = router;
